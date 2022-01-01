0b6aa6f1932ca9c1676d6a54c0f633dd7f6ba171
Mark Roxberry <mark@un2.tech>
2022-01-01 15:15:04 -0500

    Adding Amplify Auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyappf914f1cf": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
-    </div>
-  );
+function App({ isPassedToWithAuthenticator, signOut, user }) {
+    // if (!isPassedToWithAuthenticator) {
+    //     throw new Error(`isPassedToWithAuthenticator was not provided`);
+    // }
+
+    return (
+        <div className="App">
+            <header>
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1>We now have Auth! Cool</h1>
+            </header>
+            <button onClick={signOut}>Sign out</button>
+        </div>
+    );
 }
 
-export default App;
+export default withAuthenticator(App);
+
+export async function getStaticProps() {
+  return {
+    props: {
+      isPassedToWithAuthenticator: true,
+    },
+  };
+}
