b522e69fd99c6bedfaf1291653b75d354bfe758f
Mark Roxberry <mark@un2.tech>
2022-01-01 20:34:44 -0500

    Adding storage API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { API } from "aws-amplify";
+import { API, Storage } from "aws-amplify";
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 import { listNotes } from "./graphql/queries";
@@ -26,6 +26,16 @@ function App({ isPassedToWithAuthenticator, signOut, user }) {
 
     async function fetchNotes() {
         const apiData = await API.graphql({ query: listNotes });
+        const notesFromAPI = apiData.data.listNotes.items;
+        await Promise.all(
+            notesFromAPI.map(async (note) => {
+                if (note.image) {
+                    const image = await Storage.get(note.image);
+                    note.image = image;
+                }
+                return note;
+            })
+        );
         setNotes(apiData.data.listNotes.items);
     }
 
@@ -35,6 +45,10 @@ function App({ isPassedToWithAuthenticator, signOut, user }) {
             query: createNoteMutation,
             variables: { input: formData },
         });
+        if (formData.image) {
+            const image = await Storage.get(formData.image);
+            formData.image = image;
+        }
         setNotes([...notes, formData]);
         setFormData(initialFormState);
     }
@@ -46,6 +60,14 @@ function App({ isPassedToWithAuthenticator, signOut, user }) {
             query: deleteNoteMutation,
             variables: { input: { id } },
         });
+    }
+
+    async function onChange(e) {
+        if (!e.target.files[0]) return;
+        const file = e.target.files[0];
+        setFormData({ ...formData, image: file.name });
+        await Storage.put(file.name, file);
+        fetchNotes();
     }
 
     return (
@@ -65,6 +87,7 @@ function App({ isPassedToWithAuthenticator, signOut, user }) {
                 placeholder="Note description"
                 value={formData.description}
             />
+            <input type="file" onChange={onChange} />
             <button onClick={createNote}>Create Note</button>
             <div style={{ marginBottom: 30 }}>
                 {notes.map((note) => (
@@ -74,6 +97,9 @@ function App({ isPassedToWithAuthenticator, signOut, user }) {
                         <button onClick={() => deleteNote(note)}>
                             Delete note
                         </button>
+                        {
+                          note.image && <img src={note.image} style={{width: 400}} />
+                        }
                     </div>
                 ))}
             </div>
