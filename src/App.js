b4067c32f05c7d6a8fbead8bf6d442118dd9d724
Mark Roxberry <mark@un2.tech>
2022-01-01 20:00:27 -0500

    Added GraphQL backend

--- src/App.js
+++ src/App.js
@@ -1,20 +1,82 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { API } from "aws-amplify";
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
+import { listNotes } from "./graphql/queries";
+import {
+    createNote as createNoteMutation,
+    deleteNote as deleteNoteMutation,
+} from "./graphql/mutations";
+
+const initialFormState = { name: "", description: "" };
 
 function App({ isPassedToWithAuthenticator, signOut, user }) {
     // if (!isPassedToWithAuthenticator) {
     //     throw new Error(`isPassedToWithAuthenticator was not provided`);
     // }
 
+    const [notes, setNotes] = useState([]);
+    const [formData, setFormData] = useState(initialFormState);
+
+    useEffect(() => {
+        fetchNotes();
+    }, []);
+
+    async function fetchNotes() {
+        const apiData = await API.graphql({ query: listNotes });
+        setNotes(apiData.data.listNotes.items);
+    }
+
+    async function createNote() {
+        if (!formData.name || !formData.description) return;
+        await API.graphql({
+            query: createNoteMutation,
+            variables: { input: formData },
+        });
+        setNotes([...notes, formData]);
+        setFormData(initialFormState);
+    }
+
+    async function deleteNote({ id }) {
+        const newNotesArray = notes.filter((note) => note.id !== id);
+        setNotes(newNotesArray);
+        await API.graphql({
+            query: deleteNoteMutation,
+            variables: { input: { id } },
+        });
+    }
+
     return (
         <div className="App">
-            <header>
-                <img src={logo} className="App-logo" alt="logo" />
-                <h1>We now have Auth! Cool</h1>
-            </header>
+            <h1>Notes App</h1>
+            <input
+                onChange={(e) =>
+                    setFormData({ ...formData, name: e.target.value })
+                }
+                placeholder="Note name"
+                value={formData.name}
+            />
+            <input
+                onChange={(e) =>
+                    setFormData({ ...formData, description: e.target.value })
+                }
+                placeholder="Note description"
+                value={formData.description}
+            />
+            <button onClick={createNote}>Create Note</button>
+            <div style={{ marginBottom: 30 }}>
+                {notes.map((note) => (
+                    <div key={note.id || note.name}>
+                        <h2>{note.name}</h2>
+                        <p>{note.description}</p>
+                        <button onClick={() => deleteNote(note)}>
+                            Delete note
+                        </button>
+                    </div>
+                ))}
+            </div>
             <button onClick={signOut}>Sign out</button>
         </div>
     );
@@ -23,9 +85,9 @@ function App({ isPassedToWithAuthenticator, signOut, user }) {
 export default withAuthenticator(App);
 
 export async function getStaticProps() {
-  return {
-    props: {
-      isPassedToWithAuthenticator: true,
-    },
-  };
+    return {
+        props: {
+            isPassedToWithAuthenticator: true,
+        },
+    };
 }
